9ae4621ce0d702ec0f5ec60b8b4a26a60a6ff604
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2024-10-10 16:38:51 +0200

    Update StateManager.kt

--- src/main/kotlin/best/spaghetcodes/duckdueller/bot/StateManager.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/StateManager.kt
@@ -30,7 +30,7 @@ object StateManager {
         } else if (unformatted.contains("Opponent:")) {
             state = States.PLAYING
             gameStartedAt = System.currentTimeMillis()
-        } else if (unformatted.contains("Accuracy")) {
+        } else if (unformatted.contains("Damage dealt")) {
             state = States.GAME
             gameFull = false
             lastGameDuration = System.currentTimeMillis() - gameStartedAt
